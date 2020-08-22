62931f0ffa876bb2a738e421a7d5623d3497ba3a
kabeh2 <mattcnk@hotmail.com>
2020-08-22 15:23:51 -0400

    Added update mutations for users, posts, and comments

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -40,6 +40,30 @@ const Mutation = {
 
     return deletedUser[0];
   },
+  updateUser(parent, args, { db }, info) {
+    const { id, data } = args;
+    const user = db.users.find((user) => user.id === id);
+
+    if (!user) throw new Error('User not found');
+
+    if (typeof data.email === 'string') {
+      const emailTaken = db.users.some((user) => user.email === data.email);
+
+      if (emailTaken) throw new Error('Email taken.');
+
+      user.email = data.email;
+    }
+
+    if (typeof data.name === 'string') {
+      user.name = data.name;
+    }
+
+    if (typeof data.age !== 'undefined') {
+      user.age = data.age;
+    }
+
+    return user;
+  },
   createPost(parent, args, { db }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
 
@@ -66,6 +90,27 @@ const Mutation = {
     db.comments = db.comments.filter((comment) => comment.posts !== args.id);
 
     return deletedPost[0];
+  },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+
+    const post = db.posts.find((post) => post.id === id);
+
+    if (!post) throw new Error('No post exists.');
+
+    if (typeof data.title === 'string') {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === 'string') {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+    }
+
+    return post;
   },
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
@@ -97,6 +142,18 @@ const Mutation = {
     const deletedComment = db.comments.splice(commentIndex, 1);
 
     return deletedComment[0];
+  },
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find((comment) => comment.id === id);
+
+    if (!comment) throw new Error('This comment does not exist.');
+
+    if (typeof data.text === 'string') {
+      comment.text = data.text;
+    }
+
+    return comment;
   },
 };
 
